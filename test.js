ba9620543487a04d79ce9ea0a9b22bf185087599
zhangchengmcbedrock <120537551+zhangchengmcbedrock@users.noreply.github.com>
2023-05-07 21:51:55 +0800

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-    var now = new Date().getTime();
-    var page_load_time = now-performance.timing.navigationStart;
-    console.clear();
-     console.log('%cmoedog.org','font-size:2em');
-    console.log('%c页面加载完毕消耗了'+Math.round(performance.now()*100)/100+'ms','background:#fff;color:#333;text-shadow:0 0 2px #eee,0 0 3px #eee,0 0 3px #eee,0 0 2px #eee,0 0 3px #eee;');
-;
-     console.log('测试);
-};
